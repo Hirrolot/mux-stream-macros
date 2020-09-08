66349298694f14e1c0c9760e4c66e88aa3b27281
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-09-08 22:03:56 +0600

    Show that no output stream can slow down another one in demux!

--- tests/test.rs
+++ tests/test.rs
@@ -7,6 +7,7 @@ fn test() {
 
     t.pass("tests/ui/demux/works.rs");
     t.pass("tests/ui/demux/rest_pattern.rs");
+    t.pass("tests/ui/demux/output_streams_are_independent.rs");
     t.compile_fail("tests/ui/demux/fails_on_empty_input_list.rs");
     t.compile_fail("tests/ui/demux/non_exhaustive_fails.rs");
     t.compile_fail("tests/ui/demux/rest_pattern_fails.rs");
